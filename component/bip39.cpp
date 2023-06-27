c8d60ed65af74a36ce8fbeecd79a4463cfb2e0e1
Meryeme Haman <84973134+meryeme0506@users.noreply.github.com>
2023-06-27 13:56:56 +0200

    update

--- component/bip39.cpp
+++ component/bip39.cpp
@@ -1,150 +1,112 @@
-#include <stdlib.h>
-#include <stdexcept>
-#include <vector>
-#include <bitset>
 #include <string>
+#include <vector>
+#include <openssl/sha.h>
 #include <pybind11/pybind11.h>
-#include <boost/crc.hpp>
-
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
-#include "bip39.h"
-#include "bip_functions.h"
+class BIP39Encoder {
+private:
+    std::vector<std::string> wordList; 
 
-using namespace std;
+    std::string createRandomEntropy() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> dis(0, 255);
 
-vector<int> POSSIBLE_WORD_COUNT = {12, 15, 18, 21, 24};
+        std::string entropy;
+        entropy.reserve(32); // 256 bits / 8 = 32 bytes
 
-string Bip39::create_random_entropy() {
-	string random_bytes_hex = generate_random_bytes(this->entropy_length / 8);
-	this->entropy_ = random_bytes_hex;
-	return random_bytes_hex;
-}
-
-string Bip39::create_checksum(string& entropy_sequence) const {
-    string result;
-
-    // Convert entropy_sequence to binary data
-    vector<unsigned char> data(entropy_sequence.begin(), entropy_sequence.end());
-
-    // Calculate CRC32 checksum
-    boost::crc_32_type crc32;
-    crc32.process_bytes(data.data(), data.size());
-
-    // Convert checksum to binary string
-    string checksum = bitset<32>(crc32.checksum()).to_string();
-
-    // Append checksum to entropy_sequence
-    string result_to_append = checksum.substr(0, this->checksum);
-    result = entropy_sequence + result_to_append;
-
-    return result;
-}
-
-string Bip39::convert_to_recovery_phrase(string& checksum_sequence) const {
-	vector<string> v;
-	auto words_dict = get_words(this->language_);
-
-	for (int i = 0; i < checksum_sequence.length(); i += 11) {
-		auto sub = checksum_sequence.substr(i, 11);
-		auto word = words_dict[stoi(sub, nullptr, 2)];
-		v.push_back(word);
-	}
-
-	string result = accumulate(next(begin(v)), end(v), v[0],
-				[](string a, string b) {
-					return move(a) + " " + b;
-				});
-
-	return result;
-}
-
-string Bip39::create_recovery_phrase(int word_quantity) {
-	if (find(begin(POSSIBLE_WORD_COUNT), end(POSSIBLE_WORD_COUNT), word_quantity) == end(POSSIBLE_WORD_COUNT)) {
-		throw invalid_argument("The number of words wanted must be in [12, 15, 18, 21, 24] interval");
-	}
-	this->word_count = word_quantity;
-	this->checksum = (this->word_count * 11) % 32;
-	this->entropy_length = (this->word_count * 11) - this->checksum;
-
-	auto entropy_sequence = this->create_random_entropy();
-	entropy_sequence = hex_str_to_bin_str(entropy_sequence);
-	auto checksum_sequence = this->create_checksum(entropy_sequence);
-	this->mnemonic  = this->convert_to_recovery_phrase(checksum_sequence);
-	return this->mnemonic;
-}
-
-string Bip39::create_recovery_phrase_from_entropy(string given_entropy) {
-
-	this->entropy_length = given_entropy.length() * 4;
-	this->checksum = this->entropy_length / 32;
-	this->word_count = (this->entropy_length + this->checksum) / 11;
-	this->entropy_ = given_entropy;
-	if (find(begin(POSSIBLE_WORD_COUNT), end(POSSIBLE_WORD_COUNT), this->word_count) == end(POSSIBLE_WORD_COUNT)) {
-                throw invalid_argument("The entropy given doesn't have a word count in [12, 15, 18, 21, 24] interval");
+        for (int i = 0; i < 32; ++i) {
+            entropy.push_back(static_cast<char>(dis(gen)));
         }
 
-	auto entropy_sequence = hex_str_to_bin_str(given_entropy);
-	auto checksum_sequence = this->create_checksum(entropy_sequence);
-	this->mnemonic = this->convert_to_recovery_phrase(checksum_sequence);
-	return this->mnemonic;
-}
+        return entropy;
+    }
 
-string Bip39::extract_entropy(string recovery_phrase) {
-	stringstream ss(recovery_phrase);
-	string buf;
-	vector<string> words;
-	while (ss >> buf) {
-		words.push_back(buf);
-	}
-	auto words_dict = get_words(this->language_);
-	string words_string_binary;
-	this->word_count = words.size();
+    std::string createChecksum(std::string& entropy) {
+        // Calculer le hash SHA256 de l'entropie
+        unsigned char hash[SHA256_DIGEST_LENGTH];
+        SHA256(reinterpret_cast<const unsigned char*>(entropy.data()), entropy.size(), hash);
 
-	for_each(words.begin(), words.end(),
-		[&](string const& w){
-			auto it = find(words_dict.begin(), words_dict.end(), w);
-			if (it == words_dict.end()) {
-				throw invalid_argument("The word : " + w + "is not in the wordlist!");
-			}
-			words_string_binary += bitset<11>(it - words_dict.begin()).to_string();
-		});
+        // Récupérer les premiers bits du hash pour obtenir la somme de contrôle
+        int checksumLength = entropy.size() / 4; // Nombre de bits de somme de contrôle (8 bits / 32 = 2 bits)
+        std::string checksum;
+        checksum.reserve(checksumLength);
 
-	string words_without_checksum = words_string_binary.substr(0, words_string_binary.length() - this->word_count);
-	string result;
+        for (int i = 0; i < checksumLength; ++i) {
+            checksum.push_back(hash[i / 8] >> (7 - (i % 8)) & 1 ? '1' : '0');
+        }
 
-	for (auto i = 0; i < words_without_checksum.length(); i += 8){
-        	result += bin_str_to_hex_str(words_without_checksum.substr(i, 8));
-    	}
-	this->entropy_ = result;
-	return this->entropy_;
-}
+        return checksum;
+    }
 
-bool Bip39::validate_recovery_phrase(string& entropy_sequence, string& recovery_phrase) {
+    std::string convertToRecoveryPhrase(std::string& entropyWithChecksum) {
+        std::string recoveryPhrase;
+        recoveryPhrase.reserve(entropyWithChecksum.size() * 3 / 4); // Chaque groupe de 4 bits sera converti en un mot
 
-	int checksum = entropy_sequence.length() * 4 / 32;
-	int word_quantity = (checksum + entropy_sequence.length() * 4) / 11;
+        for (std::size_t i = 0; i < entropyWithChecksum.size(); i += 11) {
+            std::string bits = entropyWithChecksum.substr(i, 11);
+            std::size_t index = std::bitset<11>(bits).to_ulong();
+            recoveryPhrase += wordList[index];
+            recoveryPhrase.push_back(' ');
+        }
 
-	this->word_count = word_quantity;
-        this->checksum = checksum;
-        this->entropy_length = entropy_sequence.length() * 4;
+        // Supprimer l'espace final
+        recoveryPhrase.pop_back();
 
-	entropy_sequence = hex_str_to_bin_str(entropy_sequence);
-        auto checksum_sequence = this->create_checksum(entropy_sequence);
-        auto expected_recovery_phrase  = this->convert_to_recovery_phrase(checksum_sequence);
-	return expected_recovery_phrase.compare(recovery_phrase) == 0;
-}
+        return recoveryPhrase;    }
 
-PYBIND11_MODULE(bip39, comp) {
-    py::class_<Bip39>(comp, "Bip39")
-    .def(py::init<const string &>(), py::arg("language") = "en")
-	.def("get_language", &Bip39::get_language)
-	.def("set_language", &Bip39::set_language)
-	.def("get_recovery_phrase", &Bip39::get_recovery_phrase)
-	.def("get_entropy", &Bip39::get_entropy)
-    .def("create_recovery_phrase", &Bip39::create_recovery_phrase)
-	.def("create_recovery_phrase_from_entropy", &Bip39::create_recovery_phrase_from_entropy)
-	.def("extract_entropy", &Bip39::extract_entropy)
-	.def("validate_recovery_phrase", &Bip39::validate_recovery_phrase);
+public:
+    BIP39Encoder(std::vector<std::string> wordList) : wordList(wordList) {}
+
+    std::string createRecoveryPhrase(int wordQuantity) {
+        std::string entropy = createRandomEntropy();
+        std::string checksum = createChecksum(entropy);
+        std::string entropyWithChecksum = entropy + checksum;
+
+        return convertToRecoveryPhrase(entropyWithChecksum);
+    }
+
+    std::string createRecoveryPhraseFromEntropy(std::string givenEntropy) {
+        std::string checksum = createChecksum(givenEntropy);
+        std::string entropyWithChecksum = givenEntropy + checksum;
+
+        return convertToRecoveryPhrase(entropyWithChecksum);
+    }
+
+    std::string extractEntropy(std::string recoveryPhrase) {
+        std::string entropy;
+        entropy.reserve(recoveryPhrase.size() * 4 / 3); // Chaque mot est converti en un groupe de 4 bits
+
+        // Supprimer les espaces
+        recoveryPhrase.erase(std::remove(recoveryPhrase.begin(), recoveryPhrase.end(), ' '), recoveryPhrase.end());
+        for (std::size_t i = 0; i < recoveryPhrase.size(); i += 4) {
+            std::string word = recoveryPhrase.substr(i, 4);
+            std::size_t index = std::find(wordList.begin(), wordList.end(), word) - wordList.begin();
+            std::string bits = std::bitset<11>(index).to_string();
+            entropy += bits;
+        }
+
+        return entropy;
+    }
+
+    bool validateRecoveryPhrase(std::string& recoveryPhrase) {
+        std::string extractedEntropy = extractEntropy(recoveryPhrase);
+        std::string recreatedPhrase = createRecoveryPhraseFromEntropy(extractedEntropy);
+
+        return recoveryPhrase == recreatedPhrase;
+    }
+};
+
+PYBIND11_MODULE(bip39_component, m) {
+    m.doc() = "BIP39 Encoder/Decoder";
+
+    py::class_<BIP39Encoder>(m, "BIP39Encoder")
+        .def(py::init<std::vector<std::string>>())
+        .def("createRecoveryPhrase", &BIP39Encoder::createRecoveryPhrase)
+        .def("createRecoveryPhraseFromEntropy", &BIP39Encoder::createRecoveryPhraseFromEntropy)
+        .def("extractEntropy", &BIP39Encoder::extractEntropy)
+        .def("validateRecoveryPhrase", &BIP39Encoder::validateRecoveryPhrase);
 }
--- component/test.py
+++ component/test.py
@@ -1,59 +1,21 @@
-import pytest
-import bip39 as bip
+import bip39
 
-def test_create_random_entropy():
-    # Test entropy generation
-    b = bip.Bip39()
-    try:
-        entropy = b.create_random_entropy()
-        print("Generated entropy: ", entropy)
-    except Exception:
-        assert False, "Expected no exception when generating entropy"
+def read_word_list_from_file(file_path):
+    word_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            word_list.append(line.strip())
+    return word_list
 
-def test_create_recovery_phrase():
-    # Test with a valid number of words
-    b = bip.Bip39()
-    try:
-        recovery_phrase = b.create_recovery_phrase(12)
-        print("Generated recovery phrase: ", recovery_phrase)
-    except Exception:
-        assert False, "Expected no exception for valid word count"
+word_list = read_word_list_from_file("french.txt")
+bip39 = bip39.BIP39Encoder(word_list)
+word_quantity = 12
 
-    # Test with a number of words outside the range [12, 15, 18, 21, 24]
-    try:
-        b.create_recovery_phrase(10)
-    except ValueError:
-        print("Correctly raised ValueError for word count outside of valid range")
-    else:
-        assert False, "Expected ValueError for word count outside of valid range"
+recovery_phrase = bip39.createRecoveryPhrase(word_quantity)
+print("Recovery Phrase:", recovery_phrase)
 
-def test_create_recovery_phrase_from_entropy():
-    # Test creating recovery phrase from given entropy
-    b = bip.Bip39()
-    try:
-        entropy = b.create_random_entropy()
-        recovery_phrase = b.create_recovery_phrase_from_entropy(entropy)
-        print("Generated recovery phrase from given entropy: ", recovery_phrase)
-    except Exception:
-        assert False, "Expected no exception when creating recovery phrase from given entropy"
+entropy = bip39.extractEntropy(recovery_phrase)
+print("Extracted Entropy:", entropy)
 
-def test_extract_entropy():
-    # Test extracting entropy from recovery phrase
-    b = bip.Bip39()
-    try:
-        recovery_phrase = b.create_recovery_phrase(12)
-        extracted_entropy = b.extract_entropy(recovery_phrase)
-        print("Extracted entropy from recovery phrase: ", extracted_entropy)
-    except Exception:
-        assert False, "Expected no exception when extracting entropy from recovery phrase"
-
-def test_validate_recovery_phrase():
-    # Test validating recovery phrase
-    b = bip.Bip39()
-    try:
-        recovery_phrase = b.create_recovery_phrase(12)
-        entropy = b.extract_entropy(recovery_phrase)
-        is_valid = b.validate_recovery_phrase(entropy, recovery_phrase)
-        print("Recovery phrase validation result: ", is_valid)
-    except Exception:
-        assert False, "Expected no exception when validating recovery phrase"
+is_valid = bip39.validateRecoveryPhrase(recovery_phrase)
+print("Is Valid Recovery Phrase:", is_valid)
